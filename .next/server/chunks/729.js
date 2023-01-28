d652d710f5d24d1a2e6bcaaedc44153f0a2f5276
Yizhen Chen <jonkiky@Yizhens-MBP.fios-router.home>
2023-01-28 08:09:33 -0500

    run build

---
+++ .next/server/chunks/729.js
@@ -0,0 +1,142 @@
+"use strict";
+exports.id = 729;
+exports.ids = [729];
+exports.modules = {
+
+/***/ 3211:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "$0": () => (/* binding */ Section),
+/* harmony export */   "DK": () => (/* binding */ SubTitle),
+/* harmony export */   "Dx": () => (/* binding */ Title),
+/* harmony export */   "W2": () => (/* binding */ Container),
+/* harmony export */   "Xk": () => (/* binding */ Paper),
+/* harmony export */   "e8": () => (/* binding */ UnderLine),
+/* harmony export */   "nv": () => (/* binding */ Paragraph)
+/* harmony export */ });
+/* unused harmony exports blackBoldSpan, dot */
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7518);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const Paper = styled_components__WEBPACK_IMPORTED_MODULE_0___default().div({});
+const Container = styled_components__WEBPACK_IMPORTED_MODULE_0___default().div({
+  "width": "100%",
+  "@media (min-width: 640px)": {
+    "maxWidth": "640px"
+  },
+  "@media (min-width: 768px)": {
+    "maxWidth": "768px"
+  },
+  "@media (min-width: 1024px)": {
+    "maxWidth": "1024px"
+  },
+  "@media (min-width: 1280px)": {
+    "maxWidth": "1280px"
+  },
+  "@media (min-width: 1536px)": {
+    "maxWidth": "1536px"
+  },
+  "marginLeft": "auto",
+  "marginRight": "auto",
+  "marginTop": "0px",
+  "alignItems": "center",
+  "justifyContent": "space-between",
+  "paddingLeft": "1.5rem",
+  "paddingRight": "1.5rem",
+  "paddingTop": "0.75rem",
+  "paddingBottom": "0.75rem",
+  "lineHeight": "2.5rem"
+});
+const Title = styled_components__WEBPACK_IMPORTED_MODULE_0___default().h1({
+  "marginTop": "1rem",
+  "marginBottom": "1rem",
+  "fontSize": "1.875rem",
+  "lineHeight": "1",
+  "fontWeight": "800",
+  "--tw-text-opacity": "1",
+  "color": "rgb(17 24 39 / var(--tw-text-opacity))",
+  "@media (min-width: 768px)": {
+    "fontSize": "3.75rem",
+    "lineHeight": "1"
+  },
+  "@media (min-width: 1024px)": {
+    "fontSize": "3.75rem",
+    "lineHeight": "1"
+  }
+});
+const Paragraph = styled_components__WEBPACK_IMPORTED_MODULE_0___default().p({
+  "marginBottom": "1rem",
+  "fontSize": "1rem",
+  "lineHeight": "1.5rem",
+  "fontWeight": "400",
+  "--tw-text-opacity": "1",
+  "color": "rgb(75 85 99 / var(--tw-text-opacity))",
+  "@media (min-width: 768px)": {
+    "fontSize": "1.125rem",
+    "lineHeight": "1.75rem"
+  },
+  "@media (min-width: 1024px)": {
+    "fontSize": "1.125rem",
+    "lineHeight": "1.75rem"
+  }
+});
+const SubTitle = styled_components__WEBPACK_IMPORTED_MODULE_0___default().h2({
+  "marginTop": "1.5rem",
+  "marginBottom": "1.5rem",
+  "fontSize": "1.5rem",
+  "lineHeight": "2rem",
+  "fontWeight": "800",
+  ".dark &": {
+    "--tw-text-opacity": "1",
+    "color": "rgb(255 255 255 / var(--tw-text-opacity))"
+  },
+  "@media (min-width: 768px)": {
+    "fontSize": "2.25rem",
+    "lineHeight": "2.5rem"
+  },
+  "@media (min-width: 1024px)": {
+    "fontSize": "2.25rem",
+    "lineHeight": "2.5rem"
+  }
+});
+const UnderLine = styled_components__WEBPACK_IMPORTED_MODULE_0___default().span({
+  "textDecorationLine": "underline",
+  "textDecorationColor": "#76A9FA",
+  "textDecorationThickness": "8px",
+  "textUnderlineOffset": "4px",
+  ".dark &": {
+    "textDecorationColor": "#1C64F2"
+  }
+});
+const Section = styled_components__WEBPACK_IMPORTED_MODULE_0___default().section({
+  "marginTop": "auto",
+  "marginBottom": "auto",
+  "display": "grid",
+  "height": "100vh",
+  "gridTemplateColumns": "repeat(2, minmax(0, 1fr))",
+  "flexDirection": "row",
+  "alignContent": "center",
+  "gap": "1rem"
+});
+const blackBoldSpan = styled_components__WEBPACK_IMPORTED_MODULE_0___default().span({
+  "fontWeight": "700",
+  "--tw-text-opacity": "1",
+  "color": "rgb(15 23 42 / var(--tw-text-opacity))"
+});
+const dot = styled_components__WEBPACK_IMPORTED_MODULE_0___default().span({
+  "display": "inline-block",
+  "height": "0.5rem",
+  "width": "0.5rem",
+  "borderRadius": "9999px",
+  "--tw-bg-opacity": "1",
+  "backgroundColor": "rgb(0 0 0 / var(--tw-bg-opacity))"
+});
+
+/***/ })
+
+};
+;
