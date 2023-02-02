ad86a78b87f3f1a9578856d41867035332d08e91
Yizhen Chen <jonkiky@Yizhens-MBP.fios-router.home>
2023-02-01 23:41:57 -0500

    fix build file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
   // trim off `<owner>/`
-  const repo = "yizhen-cnc-profile"
-	const assetPrefix = `/${repo}`
-	const basePath = `/${repo}`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
   images: {
