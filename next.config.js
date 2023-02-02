fbef6624022b3618e3b56bd88e0950b128756885
Yizhen Chen <jonkiky@Yizhens-MBP.fios-router.home>
2023-02-01 23:18:37 -0500

    add build file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+  // trim off `<owner>/`
+  const repo = "yizhen-cnc-profile"
+	const assetPrefix = `/${repo}/`
+	const basePath = `/${repo}`
 
 module.exports = {
+  images: {
+        unoptimized: true
+    },
    reactStrictMode: true,
    compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
-  }
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
