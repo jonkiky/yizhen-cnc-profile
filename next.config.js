ef460b61745551f1ace4566fe458f4b44ec33aa3
Chen <jonkiky@gmail.com>
2023-02-13 22:31:12 -0500

    update cname

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
-let basePath = '/'
 
 if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  assetPrefix = `https://cnc.joniky.com/`
 }
 
 module.exports = {
@@ -21,5 +16,4 @@ module.exports = {
     styledComponents: true
   },
   assetPrefix: assetPrefix,
-  basePath: basePath,
 };
